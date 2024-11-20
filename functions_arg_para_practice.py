d7bb6851812de28fde7aed44fd177bf3320f6212
Tiffany Shao <150872296+splatface@users.noreply.github.com>
2024-11-20 11:02:16 -0500

    Add files via upload

---
+++ functions_arg_para_practice.py
@@ -0,0 +1,19 @@
+#1 --- 2 arguments, tables (4) and chairs_per_table (5), output is 20, removing an argument will give an error, adding will give an error
+
+#2
+def print_integer(taken: int):
+    print(taken)
+
+print_integer(input("INTEGER: "))
+
+#3
+def difference(a: int, b:int) -> int:
+    print(abs(a-b))
+
+difference(4, 7)
+
+#4 --- no def, no b defined in the parameters of the function
+
+#5 --- either: activate_thrusters(5), activate_thrusters(7)
+
+#6 --- no brackets to call function, should be person_apples()
